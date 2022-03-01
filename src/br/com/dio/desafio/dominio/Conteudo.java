6190e3f245f8190759e93c85c854c377dab934c2
fagnerviana <fagsantiago@gmail.com>
2022-03-01 10:26:48 -0300

    Criação da classe Conteudo

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO=10d;
+
+    //Atributos da classe
+    private String titulo;
+    private String descricao;
+    
+    //metodo
+    public abstract double calcularXp();
+
+
+    //Get and Settings
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
